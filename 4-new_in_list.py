df3e67606a95064ab8d3647a98063525899b82e2
daod2003 <1253@holbertonschool.com>
2019-12-17 16:55:05 +0000

    add 5 new files

---
+++ 4-new_in_list.py
@@ -0,0 +1,8 @@
+#/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list = None:
+        return
+    a = my_list[:]
+    if idx >= 0 or idx < len(a):
+        a[idx] = element
+    return a
