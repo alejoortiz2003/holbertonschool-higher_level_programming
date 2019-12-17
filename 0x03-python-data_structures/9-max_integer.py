fa89cbc48d86befc36f8ad130f65536f2b9ba981
daod2003 <1253@holbertonschool.com>
2019-12-17 21:39:47 +0000

    add 9-max_integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == "":
+        return None
+    for i in my_list:
+        if my_list[i] > my_list[i + 1]:
+            return my_list[i]
+        else:
+            return my_list[i + 1]
