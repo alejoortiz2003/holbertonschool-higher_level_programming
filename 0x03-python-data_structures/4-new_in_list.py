f61e5a1fc3fc73cd6a346285e9307156cc82eab7
daod2003 <1253@holbertonschool.com>
2019-12-17 17:23:06 +0000

    fixed code of 4-replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if my_list == None:
         return
-    a = my_list[:]
-    if idx >= 0 or idx < len(a):
-        a[idx] = element
-    return a
+    new_list = my_list[:]
+    if idx >= 0 or idx < len(new_list):
+        new_list[idx] = element
+    return new_list
