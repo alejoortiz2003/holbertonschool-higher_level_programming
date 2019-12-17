4296ba0ceec0e1456c6f0387ea1e29b9b9069096
daod2003 <1253@holbertonschool.com>
2019-12-17 17:40:44 +0000

    fixed code of file 3

--- 4-new_in_list.py
+++
@@ -1,8 +0,0 @@
-#/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if my_list = None:
-        return
-    a = my_list[:]
-    if idx >= 0 or idx < len(a):
-        a[idx] = element
-    return a
