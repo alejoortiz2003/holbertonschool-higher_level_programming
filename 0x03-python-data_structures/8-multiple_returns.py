393376dcbb6413427eaa03adc0d06c052f29e379
daod2003 <1253@holbertonschool.com>
2019-12-17 22:11:03 +0000

    add file 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) is 0:
-        sentence[0] = None
-    length = len(sentence)
-    return ((length, sentence[0]))
+        my_t = (len(sentence), None)
+    else:
+        my_t = (len(sentence), sentence[0])
+    return my_t
