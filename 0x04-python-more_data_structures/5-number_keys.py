55fcabe2e2ecb8952d46681604ff254438ca5b99
daod2003 <1253@holbertonschool.com>
2019-12-19 21:14:02 +0000

    add 6-print_sorted_dictionary.py and modified file number 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     idx = 0
-    for idx,i in enumerate(a_dictionary):
+    for idx, i in enumerate(a_dictionary):
         idx = idx + 1
     return idx
