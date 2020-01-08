25714f0826b46deea7c24e52a6083382b6f7e0eb
daod2003 <1253@holbertonschool.com>
2020-01-08 15:59:40 +0000

    add 0x05 directory

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,9 +3,13 @@ def roman_to_int(roman_string):
     cr = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
     if roman_string is not None or roman_string is str:
-        for i in roman_string:
-            if i in cr.keys():
-                value += cr[i]
-        return value
+        for i in range(len(roman_string)):
+            if i > 1:
+                if (cr[roman_string[i - 1]] <= cr[roman_string[i]]):
+                    value += cr[i]
+                    return value
+                else:
+                    value += cr[i]
+                return value -2
     else:
         return 0
