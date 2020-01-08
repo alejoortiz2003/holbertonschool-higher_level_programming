b4a363ca185e305af7b4a2148c10ef8f67dc424e
daod2003 <1253@holbertonschool.com>
2020-01-08 17:32:02 +0000

    fixed pep

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,10 @@
 def safe_print_list_integers(my_list=[], x=0):
-	b = 0
-	for i in range(x):
-		try:
-			print("{:d}".format(my_list[i]), end="")
-			b += 1
-		except (ValueError, TypeError):
-			continue
-	print()
-	return b
-	
+        b = 0
+        for i in range(x):
+                try:
+                        print("{:d}".format(my_list[i]), end="")
+                        b += 1
+                except (ValueError, TypeError):
+                        continue
+        print()
+        return b
