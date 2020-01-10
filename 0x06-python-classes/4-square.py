36d4e44682cce120ef939997cdc7ec68d0c108ff
daod2003 <1253@holbertonschool.com>
2020-01-10 01:30:48 +0000

    add file 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if typy(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value > 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
