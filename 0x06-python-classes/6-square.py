f1dc2cf38df1c0e4f35a42dd88269bbcebc648e7
daod2003 <1253@holbertonschool.com>
2020-01-10 02:52:52 +0000

    fixed code from file6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
@@ -25,21 +25,21 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for j in range(self.__position[1]):
+        for i in range(self.__position[1]):
             print()
-        for i in range(self.__size):
+        for j in range(self.__size):
             print("".join([" " for k in range(self.__position[0])]), end="")
-            print("".join(["#" for m in range(self.__size)]))
+            print("".join(["#" for l in range(self.__size)]))
 
     @property
     def position(self):
         return self.__position
 
     @position.setter
-    def position(selft, value):
-        if type(value) is not tuple or len(value) != 2 or\
-           type(value[0]) is not int or value[0] < 0 or\
-           type(value[1]) is not int or value[1] <0:
-            raise TypeError("poisition must be a tuple of 2 positive integer")
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
