11a23c1fd0afb04563bac50aa8d7fbd648d4b863
daod2003 <1253@holbertonschool.com>
2020-01-14 23:29:22 +0000

    second commit

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -27,14 +27,35 @@ class Rectangle:
     
     @property
     def height(self):
-        """private instance heigth"""
+        """private instance height"""
         return self.__height
 
-    @heigth.setter
+    @height.setter
     def height(self, value):
-        """configuration for private attributes of heigth"""
+        """configuration for private attributes of height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Function that print a rectangle filled with "#" """
+        if self.area() == 0:
+            return ""
+        return '\n'.join(["#" * self.__width] * self.__height)
+
+    def __repr__(self):
+        """Function that return a replicate of rectangle"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
