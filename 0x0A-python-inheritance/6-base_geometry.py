c6d8c1a336b573786526ad5191bff24b08627087
daod2003 <1253@holbertonschool.com>
2020-01-20 23:49:37 +0000

    add file 6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""here you can find the class BaseGeometry"""
+
+
+class BaseGeometry():
+    """here create the class"""
+    def area(self):
+        """Exception msg"""
+        raise Exception("area() is not implemented")
