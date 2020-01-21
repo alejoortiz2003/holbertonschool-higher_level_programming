60a292c4d61f3edaf5117e49d99923097df18761
daod2003 <1253@holbertonschool.com>
2020-01-21 00:41:15 +0000

    add file 11-square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""here you can find the class BaseGeometry"""
+
+
+class Square(Rectangle):
+    """here create the class"""
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))        
+
