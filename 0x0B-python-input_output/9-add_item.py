a1c6672a35c0711d53c05ad1e31e305207970899
daod2003 <1253@holbertonschool.com>
2020-01-22 01:18:36 +0000

    add 9-add_item

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that adds arguments
+"""
+
+
+from sys import argv
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+try:
+    f_json = load_from_json_file(filename)
+except:
+    f_json = []
+
+for i in argv[:1]:
+    f_json.append(i)
+
+save_to_json_file(f_json, filename)
