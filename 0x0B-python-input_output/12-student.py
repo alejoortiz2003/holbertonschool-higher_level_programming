70ddc40110934564da90f072bffe361eb445653c
daod2003 <1253@holbertonschool.com>
2020-01-22 02:09:04 +0000

    add file 12-stundet

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Here you can find the class "Student"
+"""
+
+
+class Student:
+    """Here init the class"""
+    def __init__(self, first_name, last_name, age):
+        """here init the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return the instance of the directory"""
+        if attrs is None:
+            return self.__dict__
+        _dict = {}
+        for i in attrs:
+            try:
+                _dict[i] = self.__dict__[i]
+            except:
+                pass
+        return _dict
+
