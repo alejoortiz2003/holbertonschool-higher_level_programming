e281eee252313f088c5ab3c534ccdd73c588f5a0
daod2003 <1253@holbertonschool.com>
2020-03-02 23:07:24 +0000

    add 101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
---lays the average temperature (Fahrenheit) by city ordered by
---temperature (descending).
+-- dislays the average temperature (Fahrenheit) by city ordered by
+-- temperature (descending).
 
 SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
