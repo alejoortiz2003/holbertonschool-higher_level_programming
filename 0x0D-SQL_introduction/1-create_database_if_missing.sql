1b1aa28aa9feeb204b8cd0e2c10a62b7e74fef61
daod2003 <1253@holbertonschool.com>
2020-03-02 17:36:14 +0000

    add file 1-create a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- creates the databases hbtn_0c_0 in MySQL server
+-- if the databases hbtn_0c_0 already exists, your script not fail
+-- Not use SELECT or SHOW
+
+CREATE DATABASE IF NOT EXIST hbtn_0c_0;
