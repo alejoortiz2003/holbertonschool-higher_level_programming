d195a603043f335a6a6f204d33634035574a621d
daod2003 <1253@holbertonschool.com>
2020-03-02 20:54:07 +0000

    modified indentation from file 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,4 @@
 -- Inserts a new row in the table first_table (database hbtn_0c_0)
 -- New row: id=89 and name=holberton school
 
-INSERT INTO `first_table`(ìd`, `name`)
-       VALUES(89, 'Holberton School');
+INSERT INTO `first_table`(ìd`, `name`)VALUES(89, 'Holberton School');
