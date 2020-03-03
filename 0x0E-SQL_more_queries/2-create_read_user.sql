9c3bb36e8e9bb63374c9857884ce41b118cd1976
daod2003 <1253@holbertonschool.com>
2020-03-03 14:49:24 -0500

    change named of the table in file 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_02_d'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.*
-TO 'user_02_d'@'localhost';
+TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
