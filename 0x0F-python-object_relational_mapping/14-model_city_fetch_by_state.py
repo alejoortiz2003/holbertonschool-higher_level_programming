39b20aa1b6e2883928e397656f7051a297aa071a
daod2003 <1253@holbertonschool.com>
2020-03-30 05:10:09 +0000

    add import model_city

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
+from model_city import City
 
 
 if __name__ == '__main__':
